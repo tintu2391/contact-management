20a8ef863a777466aaa74bddec0db7d1c0fc40f9
Tintu Jacob Shaji <tshaji@atlassian.com>
2019-09-03 13:42:28 -0700

    initial commit

---
+++ src/main/resources/queries/postOperation.sql
@@ -0,0 +1,1 @@
+INSERT INTO CONTACTS (ID ,FIRST_NAME,LAST_NAME, DOB ,GENDER ,TITLE, ADDRESS, COMMUNICATION)  VALUES (11,'Bob','Frederick','06/21/1980','M','Manager','blah blah St','1 a','Somewhere','WV','12345','1234',home,email',bfe@sample.com,'true)
